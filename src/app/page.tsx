1b20a45a9cc7e665112c9fd29283cc6f3dbaa91e
astromarvs <marviquint.bahio11@gmail.com>
2025-03-30 01:50:21 +0800

    updated apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.2 }}
       >
-        Hello, I'm
+        Hello, I&apos;m
       </motion.h5>
       
       {/* Name */}
